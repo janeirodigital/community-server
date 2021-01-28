c55aba82299a5757d607fc7f6c1424489c701058
Joachim Van Herwegen <joachimvh@gmail.com>
2021-01-28 15:12:57 +0100

    test: Add acl edge case tests

--- test/integration/LdpHandlerWithAuth.test.ts
+++ test/integration/LdpHandlerWithAuth.test.ts
@@ -1,6 +1,12 @@
-import { createReadStream } from 'fs';
+import { createReadStream, promises as fsPromises } from 'fs';
 import type { HttpHandler, Initializer, ResourceStore } from '../../src/';
-import { LDP, BasicRepresentation, joinFilePath } from '../../src/';
+import {
+  LDP,
+  BasicRepresentation,
+  joinFilePath,
+  ensureTrailingSlash,
+  isSystemError,
+} from '../../src/';
 import { AclHelper, ResourceHelper } from '../util/TestHelpers';
 import { BASE, getTestFolder, createFolder, removeFolder, instantiateFromConfig } from './Config';
 
@@ -22,6 +28,7 @@ describe.each(stores)('An LDP handler with auth using %s', (name, { storeUrn, se
   let handler: HttpHandler;
   let aclHelper: AclHelper;
   let resourceHelper: ResourceHelper;
+  let store: ResourceStore;
 
   beforeAll(async(): Promise<void> => {
     // Set up the internal store
@@ -39,7 +46,6 @@ describe.each(stores)('An LDP handler with auth using %s', (name, { storeUrn, se
 
     // Create and initialize the HTTP handler and related components
     let initializer: Initializer;
-    let store: ResourceStore;
     const instances = await instantiateFromConfig(
       'urn:solid-server:test:Instances',
       'ldp-with-auth.json',
@@ -136,5 +142,67 @@ describe.each(stores)('An LDP handler with auth using %s', (name, { storeUrn, se
       Buffer.from('data'),
     );
     expect(response.statusCode).toBe(401);
+  });
+
+  it('supports file paths with spaces.', async(): Promise<void> => {
+    // Specific problem for file store, so only do this if the rootFilePath folder exists
+    try {
+      await fsPromises.lstat(rootFilePath);
+    } catch (error: unknown) {
+      if (isSystemError(error) && error.code === 'ENOENT') {
+        return;
+      }
+      throw error;
+    }
+    // Correct identifier when there are spaces
+    const identifier = { path: `${ensureTrailingSlash(BASE)}with%20spaces.txt` };
+
+    // Set acl specifically for this identifier
+    const acl = `@prefix acl: <http://www.w3.org/ns/auth/acl#>.
+@prefix foaf: <http://xmlns.com/foaf/0.1/>.
+<#authorization>
+    a               acl:Authorization;
+    acl:agentClass  foaf:Agent;
+    acl:mode        acl:Read;
+    acl:accessTo    <${identifier.path}>.`;
+
+    // Prevent other access to make sure there is no hierarchy bug
+    await aclHelper.setSimpleAcl({ read: false, write: false, append: false }, 'agent');
+
+    // Write files manually to make sure there are actually spaces
+    await fsPromises.writeFile(joinFilePath(rootFilePath, 'with spaces.txt.acl'), acl);
+    await fsPromises.writeFile(joinFilePath(rootFilePath, 'with spaces.txt'), 'valid data');
+
+    // GET file
+    const response = await resourceHelper.getResource(identifier.path);
+    expect(response._getBuffer().toString()).toContain('valid data');
+  });
+
+  it('supports acl files where the identifiers dont use our normalization.', async(): Promise<void> => {
+    // Set acl to prevent accidental hierarchical assumptions
+    await aclHelper.setSimpleAcl({ read: false, write: false, append: false }, 'agent');
+
+    // Identifier with escaped character
+    const identifier = { path: `${ensureTrailingSlash(BASE)}%26foo` };
+
+    // Set acl where this identifier is not escaped (& instead of %26)
+    const acl = `@prefix acl: <http://www.w3.org/ns/auth/acl#>.
+@prefix foaf: <http://xmlns.com/foaf/0.1/>.
+<#authorization>
+    a               acl:Authorization;
+    acl:agentClass  foaf:Agent;
+    acl:mode        acl:Read;
+    acl:accessTo    <${ensureTrailingSlash(BASE)}&foo>.`;
+
+    // Prevent other access to make sure there is no hierarchy bug
+    await aclHelper.setSimpleAcl({ read: false, write: false, append: false }, 'agent');
+
+    // Write acl and resource directly to store
+    await store.setRepresentation({ path: `${identifier.path}.acl` }, new BasicRepresentation(acl, 'text/turtle'));
+    await store.setRepresentation({ path: `${identifier.path}` }, new BasicRepresentation('data', 'text/plain'));
+
+    // GET file
+    const response = await resourceHelper.getResource(identifier.path);
+    expect(response.statusCode).toBe(200);
   });
 });
